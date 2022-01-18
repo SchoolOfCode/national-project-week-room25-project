3a4cdd5e9669199157975fe11cbc148ebccca41c
SamRapley97 <samrapley@protonmail.com>
2022-01-18 10:35:00 +0000

    Some boxes created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello</header>
+      <Box
+        style={{ marginLeft: "40%", marginTop: "60px", width: "30%" }}
+        color="white"
+        bgcolor="palevioletred"
+        p={1}
+      >
+        Unsolved Requests
+      </Box>
+
+      <Box
+        style={{ marginLeft: "40%", marginTop: "60px", width: "30%" }}
+        color="red"
+        bgcolor="palevioletred"
+        p={1}
+      >
+        Main box
+      </Box>
+
+      <Box
+        style={{ marginLeft: "40%", marginTop: "60px", width: "30%" }}
+        color="blue"
+        bgcolor="palevioletred"
+        p={1}
+      >
+        Solved requests
+      </Box>
     </div>
   );
 }

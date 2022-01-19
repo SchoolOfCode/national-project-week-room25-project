02a2ba183d3b009b1ad153b25b6d38f2060aa18b
SamRapley97 <samrapley@protonmail.com>
2022-01-19 12:27:14 +0000

    created routes

--- backend/model/response.js
+++ backend/model/response.js
@@ -1,16 +1,15 @@
 import query from "../db/index.js";
 
-
-export async function getAllResponse(){
-  const sqlString = `SELECT * FROM response`
-  const res = await query(sqlString)
-  return res
+export async function getAllResponse() {
+  const sqlString = `SELECT * FROM response`;
+  const res = await query(sqlString);
+  return res;
 }
 
-export async function createResponse(item){
-  const sqlString = `INSERT INTO response () VALUES `
-  const res =  await query(sqlString)
-  return res
+export async function createResponse(response) {
+  const sqlString = `INSERT INTO response () VALUES `;
+  const res = await query(sqlString);
+  return res;
 }
 
 export async function getResponseById(id) {
--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -1,24 +1,30 @@
 import express from "express";
-import { creatRequest, getRequests, getRequestById } from "../model/requests.js";
+import {
+  creatRequest,
+  getRequests,
+  getRequestById,
+} from "../model/requests.js";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.post("/",  async function (req, res) {
-    const requestItem = await creatRequest()
- 
+router.post("/", async function (req, res) {
+  console.log("working");
+  const requestToAdd = req.body;
+  console.log(requestToAdd);
+  const requestItem = await creatRequest(requestToAdd);
+  res.json({ Payload: requestItem.rows });
 });
 
-router.get("/",  async function( req, res){
- const allRequests = await getRequests()
-res.json({Payload: allRequests.rows})
+router.get("/", async function (req, res) {
+  const allRequests = await getRequests();
+  res.json({ Payload: allRequests.rows });
+});
 
-})
-
-router.get("/:id", async function(req, res){
-    const id = Number(req.params.id)
-    const selectedRequest = await getRequestById(id)
-    res.json({Payload:selectedRequest.rows})
-})
+router.get("/:id", async function (req, res) {
+  const id = Number(req.params.id);
+  const selectedRequest = await getRequestById(id);
+  res.json({ Payload: selectedRequest.rows });
+});
 
 export default router;

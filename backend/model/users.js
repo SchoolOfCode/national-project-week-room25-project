3e1d8c5df999e759c6bcfa1102ebc30c0666bc2e
adamekissa <adamekissa@gmail.com>
2022-01-20 10:22:44 +0000

    Create response done

--- backend/model/users.js
+++ backend/model/users.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import query from "../db/index.js";
 
 export async function getUsers(){
@@ -8,6 +9,16 @@ export async function getUsers(){
 
 export async function getUserById(id){
     const sqlString = `SELECT * FROM users WHERE user_id = ${id}`
+    const res = await query(sqlString)
+    return res
+}
+
+export async function createUser(user){
+    const firstName = user.first_name;
+    const lastName = user.last_name;
+    const email = user.email;
+    const role = user.role;
+    const sqlString = `INSERT INTO users (first_name, last_name, email, role) VALUES ('${firstName}', '${lastName}', '${email}', '${role}');`;
     const res = await query(sqlString)
     return res
 }

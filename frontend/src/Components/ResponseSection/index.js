8ffa95babfff33536c4967d2abce8b97ebc37163
tommiesten <94461231+tommiesten@users.noreply.github.com>
2022-01-20 15:46:14 +0000

    started work on CSS comments

--- frontend/src/Components/ResponseSection/index.js
+++ frontend/src/Components/ResponseSection/index.js
@@ -22,36 +22,52 @@ function ResponseSection({ id, currentUserId }) {
 
   async function addResponseToList(response) {
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
     //response.body to access user's input
     // make a fetch post request to "https://week-project.herokuapp.com/response"
     const res = await fetch("https://week-project.herokuapp.com/response", {
-      method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            "user_id": currentUserId,
-            "request_id": id,
-            "body": response.body,
-            "response_date": date,
-            "vote_count": 0
-          })
-    })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: currentUserId,
+        request_id: id,
+        body: response.body,
+        response_date: date,
+        vote_count: 0,
+      }),
+    });
     const json = await res.json();
     setResponsesList([...responsesList, json.Payload[0]]);
   }
 
   function toggleResponsesVisibility() {
     setAreResponsesVisible(!areResponsesVisible);
-}
+  }
 
   return (
     <div>
-      <button onClick={toggleResponsesVisibility}>{ areResponsesVisible ? "Hide Responses" : "View Responses"}</button>
-      <div style={areResponsesVisible ? {visibility: "visible", height: "auto"} : {visibility : "hidden", height: 0}}>
+      <button className="btn btn-primary" onClick={toggleResponsesVisibility}>
+        {areResponsesVisible ? "Hide Responses" : "View Responses"}
+      </button>
+      <div
+        style={
+          areResponsesVisible
+            ? { visibility: "visible", height: "auto" }
+            : { visibility: "hidden", height: 0 }
+        }
+      >
         <ResponseList responsesList={responsesList} />
-        <ResponseForm currentUserId={currentUserId} addResponseToList={addResponseToList} />
+        <ResponseForm
+          currentUserId={currentUserId}
+          addResponseToList={addResponseToList}
+        />
       </div>
     </div>
   );

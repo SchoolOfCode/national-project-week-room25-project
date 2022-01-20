9196e9454dd73a32a14309e72c80314dc3d9b3ce
casey <cassandra.d.mclellan@gmail.com>
2022-01-20 14:15:11 +0000

    FEAT: made up vote button

--- frontend/src/Components/Response/index.js
+++ frontend/src/Components/Response/index.js
@@ -1,8 +1,28 @@
+import {useState} from "react";
+
 function Response({ response }) {
+
+  const [currentVoteCount, setCurrentVoteCount] = useState(response.vote_count);
+
+  async function handleUpVote() {
+    const res = await fetch(`https://week-project.herokuapp.com/response/${response.response_id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "vote_count": 1
+      })
+    })
+    const json = await res.json();
+    setCurrentVoteCount(json.Payload[0].vote_count);
+  }
+
   return (
     <div>
       <p>{response.response_date}</p>
-      <p>{response.vote_count}</p>
+      <p>{currentVoteCount}</p>
+      <button onClick={handleUpVote}>Up Vote</button>
       <p>{response.body}</p>
     </div>
   );

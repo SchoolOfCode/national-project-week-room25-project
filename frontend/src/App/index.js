5e0341658b02d6d618d9dcc6976b7d07716492ec
SamRapley97 <samrapley@protonmail.com>
2022-01-20 09:32:38 +0000

    some CSS styling added

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -4,6 +4,7 @@ import CreateRequest from "../Components/CreateRequests";
 import BrowseRequest from "../Components/BrowseRequests";
 import Request from "../Components/Request";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function App() {
   const [requestList, setRequestList] = useState([]);
@@ -20,23 +21,35 @@ function App() {
     <div className="myApp">
       <header className="App-header">CamperOverflow</header>
 
-      <CreateRequest />
+      <div class="topContainer">
+        <div class="row">
+          <div class="col" id="createRequest">
+            <CreateRequest />
+          </div>
 
-      <BrowseRequest />
+          <div class="col" id="searchRequest">
+            <BrowseRequest />
+          </div>
+        </div>
+      </div>
 
-      {requestList.map((request) => {
-        return (
-          <Request
-            key={request["request_id"]}
-            title={request.title}
-            body={request.body}
-            category={request.category}
-            date={request["request_date"]}
-            room={request.room}
-            userId={request["user_id"]}
-          />
-        );
-      })}
+      <div class="row">
+        {requestList.map((request) => {
+          return (
+            <div class="col" id="displayedRequest">
+              <Request
+                key={request["request_id"]}
+                title={request.title}
+                body={request.body}
+                category={request.category}
+                date={request["request_date"]}
+                room={request.room}
+                userId={request["user_id"]}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- frontend/src/Components/CreateRequests/index.js
+++ frontend/src/Components/CreateRequests/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Box from "@material-ui/core/Box";
+// import Box from "@material-ui/core/Box";
 // import Button from "react-bootstrap/Button";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -35,58 +35,50 @@ function CreateRequest() {
 
   return (
     <div>
-      <Box
-        style={{ marginLeft: "40%", marginTop: "60px", width: "30%" }}
-        color="red"
-        bgcolor="palevioletred"
-        p={1}
-      >
-        {" "}
-        <form onSubmit={handleSubmit}>
-          <h2>create a request</h2>
-          <label htmlFor="room">room#</label>
-          <input
-            type="number"
-            id="room"
-            onChange={handleOnChange}
-            required
-          ></input>
-          <label htmlFor="problemTitle">what is your problem</label>
-          <input
-            type="text"
-            id="problemTitle"
-            onChange={handleOnChange}
-            required
-          ></input>
-          <label htmlFor="category">Category</label>
-          <select id="category" onChange={handleOnChange} required>
-            <option defaultValue>miscellaneous</option>
-            <option>react</option>
-            <option>github</option>
-            <option>JavaScript</option>
-            <option>Deployment</option>
-            <option>planning</option>
-          </select>
-          <label htmlFor="discription">
-            Explain in more detail, Include what you have tried
-          </label>
-          <input
-            type="text"
-            id="discription"
-            onChange={handleOnChange}
-            required
-          ></input>
-          <label htmlFor="help">what assistance do you require?</label>
-          <select id="help" onChange={handleOnChange} required>
-            <option defaultValue>respond here</option>
-            <option>Come to room</option>
-          </select>
-          <br></br>
-          <button type="button" className="btn btn-success">
-            Success
-          </button>
-        </form>
-      </Box>
+      <form onSubmit={handleSubmit}>
+        <h2>create a request</h2>
+        <label htmlFor="room">room#</label>
+        <input
+          type="number"
+          id="room"
+          onChange={handleOnChange}
+          required
+        ></input>
+        <label htmlFor="problemTitle">what is your problem</label>
+        <input
+          type="text"
+          id="problemTitle"
+          onChange={handleOnChange}
+          required
+        ></input>
+        <label htmlFor="category">Category</label>
+        <select id="category" onChange={handleOnChange} required>
+          <option defaultValue>miscellaneous</option>
+          <option>react</option>
+          <option>github</option>
+          <option>JavaScript</option>
+          <option>Deployment</option>
+          <option>planning</option>
+        </select>
+        <label htmlFor="discription">
+          Explain in more detail, Include what you have tried
+        </label>
+        <input
+          type="text"
+          id="discription"
+          onChange={handleOnChange}
+          required
+        ></input>
+        <label htmlFor="help">what assistance do you require?</label>
+        <select id="help" onChange={handleOnChange} required>
+          <option defaultValue>respond here</option>
+          <option>Come to room</option>
+        </select>
+        <br></br>
+        <button type="button" className="btn btn-success">
+          Success
+        </button>
+      </form>
     </div>
   );
 }

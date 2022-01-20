70b25801a097a44ea02e41a264b731a4e6e16460
casey <cassandra.d.mclellan@gmail.com>
2022-01-20 16:51:30 +0000

    STYLE: added scroll for responses section

--- frontend/src/Components/ResponseList/index.js
+++ frontend/src/Components/ResponseList/index.js
@@ -2,7 +2,7 @@ import Response from "../Response";
 
 function ResponseList({ responsesList }) {
   return (
-    <div>
+    <div className="response-list">
       {responsesList.map((response) => {
         return <Response key={response.response_id} response={response} />;
       })}
